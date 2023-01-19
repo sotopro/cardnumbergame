04f2da6c492d505c26c917988b3e659df1c9f5e9
sotopro <dasg.1993@gmail.com>
2023-01-18 19:00:44 -0500

    [setup] - eslint conifg universe and prettier

---
+++ src/components/header/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#97A1D8",
+    height: 120,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+});
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { View } from "react-native";
 
-import { styles } from './styles';
+import { Header } from "./components";
+import { styles } from "./styles";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title="Adivina el numero" />
     </View>
   );
-}
-
-
+};
 
 export default App;

a75eb25701fdde1afe87254a874292e6d9097d69
sotopro <dasg.1993@gmail.com>
2023-01-18 19:15:18 -0500

    [feature] - create start game screen

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import { View } from "react-native";
 
 import { Header } from "./components";
+import { StartGame } from "./screens";
 import { styles } from "./styles";
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Header title="Adivina el numero" />
+      <StartGame />
     </View>
   );
 };
---
+++ src/screens/start-game/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { styles } from "./styles";
+
+export const StartGame = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Start Game</Text>
+    </View>
+  );
+};
+
+export default StartGame;

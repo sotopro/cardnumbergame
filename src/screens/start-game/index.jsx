2a4a604f06829aae7ecfb7d342094d4a3842be21
sotopro <dasg.1993@gmail.com>
2023-01-18 20:33:48 -0500

    [feature] - add keyboard dismiss and add state to handle input

--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -1,23 +1,39 @@
-import React from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, TouchableWithoutFeedback, Keyboard } from "react-native";
 
 import { styles } from "./styles";
 import { Card } from "../../components";
 import { colors } from "../../constants";
 
 export const StartGame = () => {
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const onHandlerChange = (text) => {
+    setEnteredValue(text.replace(/[^0-9]/g, ""));
+  };
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Comenzar juego</Text>
-      <Card style={styles.inputContainer}>
-        <Text style={styles.label}>Escribe un numero</Text>
-        <TextInput keyboardType="numeric" style={styles.input} placeholder="0" />
-        <View style={styles.buttonContainer}>
-          <Button title="Reiniciar" onPress={() => null} color={colors.secondary} />
-          <Button title="Confirmar" onPress={() => null} color={colors.primary} />
-        </View>
-      </Card>
-    </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Comenzar juego</Text>
+        <Card style={styles.inputContainer}>
+          <Text style={styles.label}>Escribe un numero</Text>
+          <TextInput
+            value={enteredValue}
+            keyboardType="numeric"
+            style={styles.input}
+            placeholder="0"
+            onChangeText={onHandlerChange}
+          />
+          <View style={styles.buttonContainer}>
+            <Button title="Reiniciar" onPress={() => null} color={colors.secondary} />
+            <Button title="Confirmar" onPress={() => null} color={colors.primary} />
+          </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 

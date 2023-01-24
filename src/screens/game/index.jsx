5f70f44d4d24c571f17c5639fabb615b05e133a4
sotopro <dasg.1993@gmail.com>
2023-01-23 19:54:32 -0500

    [feature] - create layout game screen and add function to generate random number in game screen

--- src/screens/game/index.jsx
+++ src/screens/game/index.jsx
@@ -1,12 +1,33 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button } from "react-native";
 
 import { styles } from "./styles";
+import { Card, NumberContainer } from "../../components";
+import { colors } from "../../constants";
+
+const generateRandomNumber = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const randomNumber = Math.floor(Math.random() * (max - min) + min);
+  if (randomNumber === exclude) {
+    return generateRandomNumber(min, max, exclude);
+  } else {
+    return randomNumber;
+  }
+};
 
 const Game = ({ selectedNumber }) => {
+  const [currentGuess, setCurrentGuess] = useState(generateRandomNumber(1, 100, selectedNumber));
   return (
     <View style={styles.container}>
-      <Text>Game Screen</Text>
+      <Card style={styles.content}>
+        <Text style={styles.title}>Numero del oponente</Text>
+        <NumberContainer number={currentGuess} />
+        <View style={styles.buttonContainer}>
+          <Button title="Menor" onPress={() => {}} color={colors.primary} />
+          <Button title="Mayor" onPress={() => {}} color={colors.primary} />
+        </View>
+      </Card>
     </View>
   );
 };
--- src/screens/game/styles.js
+++ src/screens/game/styles.js
@@ -6,5 +6,21 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.white,
+    alignItems: "center",
+    paddingVertical: 20,
+  },
+  content: {
+    width: "75%",
+    height: 200,
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  title: {
+    fontSize: 18,
+  },
+  buttonContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
 });
